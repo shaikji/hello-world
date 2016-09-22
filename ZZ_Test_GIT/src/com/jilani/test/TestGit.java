f4697ea7c5f79d2988ec90778fceaf41907e4829
sjilani <sjilani2014@gmail.com>
2016-09-22 15:09:00 +0530

    new java project

---
+++ ZZ_Test_GIT/src/com/jilani/test/TestGit.java
@@ -0,0 +1,11 @@
+package com.jilani.test;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Hello World!");
+	}
+
+}
